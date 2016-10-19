bfed929820bf8970f21df55c50451bbe71d16577
Aaron Evans <aaron.eavns@hart.com>
2016-10-18 17:18:09 -0700

    add login test for UnirestClarityDriver

--- src/test/java/clarity/api/unirest/UnirestClarityDriverTest.java
+++ src/test/java/clarity/api/unirest/UnirestClarityDriverTest.java
@@ -21,5 +21,14 @@ public class UnirestClarityDriverTest extends ClarityTestCase
 	public void should_login() throws Exception
 	{
 		clarity.login(validUser);
+
+		String user_id = clarity.user.user_id;
+		String x_access_token = clarity.user.x_access_token;
+
+		log.write("user_id: " + user_id);
+		log.write("x_access_token: " + x_access_token);
+
+		assertThat(user_id).isNotEmpty();
+		assertThat(x_access_token).isNotEmpty();
 	}
 }
